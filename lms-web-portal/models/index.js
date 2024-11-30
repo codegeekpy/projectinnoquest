478a42d613a9f4fb36a69c5ae000d6b51dcb2c89
codegeekpy <mechastro768@gmail.com>
2024-11-30 10:56:06 +0530

    fixing errors

--- lms-web-portal/models/index.js
+++ lms-web-portal/models/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(path.join(__dirname, '../config/config.json'))[env];
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
@@ -27,5 +27,6 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
 
 
