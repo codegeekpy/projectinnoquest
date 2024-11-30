2232e68bdcfe11354446c9aeeb046772410fb51f
sai-sriram23 <saisrirambussa999@gmail.com>
2024-11-30 11:40:31 +0530

    updated

--- lms-web-portal/models/index.js
+++ lms-web-portal/models/index.js
@@ -3,10 +3,15 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(path.join(__dirname, '../config/config.json'))[env];
+const config = require(path.resolve(__dirname, '../config/config.json'))[env];
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs.readdirSync(__dirname)
   .filter(file => {
@@ -27,6 +32,7 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
 
 
 
